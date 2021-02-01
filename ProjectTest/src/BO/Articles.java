672e99299bdee3a570b6b786f23c5e9b373c88dd
ONUCharon <47871394+ONUCharon@users.noreply.github.com>
2021-02-01 09:29:03 +0100

    ajout du BO, articles

---
+++ ProjectTest/src/BO/Articles.java
@@ -0,0 +1,15 @@
+package BO;
+
+public class Articles {
+ private int nombre;
+
+public int getNombre() {
+	return nombre;
+}
+
+public void setNombre(int nombre) {
+	this.nombre = nombre;
+}
+ 
+ 
+}
